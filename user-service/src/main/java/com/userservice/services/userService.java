d667963f1daa0e707cba84f1fd9a31bb833a34d9
Vishal Kushwaha <vishalkushwaha3637@gmail.com>
2026-03-24 20:17:56 +0530

    Add user service interface with methods for user management

--- user-service/src/main/java/com/userservice/services/userService.java
+++ user-service/src/main/java/com/userservice/services/userService.java
@@ -1,7 +1,18 @@
 package com.userservice.services;
 
-import org.springframework.stereotype.Service;
 
-@Service
+import com.userservice.model.Users;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+
 public interface userService {
+    public void saveUser(Users users);
+
+    public String getUser(String userId);
+
+    public List<Users> getAllUsers();
+
+    public void deleteUserById(String userId);
 }
